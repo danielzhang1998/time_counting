da6e77f6034bb1e9b02de19b929cde0eebdcf8eb
danielzhang1998 <63471153+danielzhang1998@users.noreply.github.com>
2020-12-11 20:10:39 +0800

    add easyGui module

--- time_adding.py
+++ time_adding.py
@@ -1,4 +1,5 @@
 import pickle
+import easygui as g
 
 total_time = 30*60*60 + 19 * 60 + 45    # total time we need to use
 sum_second = 0  # time already used, initial be zero
@@ -8,14 +9,14 @@ def data_set_read():
     my_list2 = pickle.load(pickle_file)
     calculate(my_list2)
 
-
 # calculate the progress
 def calculate(my_list2):
     global sum_second
     for each in my_list2:
         sum_second += 60 * int(each[0]) + int(each[1])
-    print("miniutes already take: " + str(sum_second/(60*60)) + "/" + str(total_time/(60*60)) + ", " 
-        + str((sum_second/(60*60))*100/(total_time/(60*60))) + " %")
+    string1 = "hours already take: " + str(sum_second/(60*60)) + " / " + str(total_time/(60*60))
+    string2 = "process: " + str((sum_second/(60*60))*100/(total_time/(60*60))) + " %"
+    g.msgbox(string1 + '\n\n' + string2)
 
 data_set_read()
 
--- timing_data_set.py
+++ timing_data_set.py
@@ -1,4 +1,5 @@
 import pickle
+import easygui as g
 
 # 18 is minutes and 00 is seconds
 # add in more time list or edit if need
@@ -11,7 +12,7 @@ my_time = [
     [19,3],[12,46],[20,48],[18,20],
     [13,26],[17,51],[17,49],[9,43],
     [16,55],[14,43],[23,33],[12,24],
-    [12,38]
+    [12,38],[15,4],[9,37],[14,16]
     ]
 
 # make a new binary file to store the data in list my_time
@@ -25,6 +26,8 @@ def data_set_read():
     pickle_file = open('learn_progress.testing','rb')   # rb is read binary
     my_list2 = pickle.load(pickle_file) # load the binary data
     print(my_list2) # show the data
+    length_data_set = len(my_list2)
+    g.msgbox('data set insert successful :)' + '\n\n' + 'total insert: ' + str(length_data_set))
 
 add_data_set(my_time)
 data_set_read()
